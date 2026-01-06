1cd52d4eff25f039906bc4f0968065bb34100d6e
Chris Kleijn <kleijn@salure.nl>
2026-01-06 12:39:12 +0100

    Updated test

--- src/__tests__/Afas.test.ts
+++ src/__tests__/Afas.test.ts
@@ -1,12 +1,11 @@
-const {Profit} = require('../../lib/index')
-const { Languages, EnvTypes } = require('afas-connect/lib/models');
+import { Profit, Languages, EnvTypes } from '../index.js';
 
 const ProfitService = new Profit({
   token: '<YOUR_TOKEN_HERE>',
   env: '12345',
   envType: EnvTypes.Production,
-  language: Languages.Dutch
-})
+  language: Languages.Dutch,
+});
 
 test('Afas config', () => {
   expect(ProfitService.config).toStrictEqual({ environment: '12345', environmentType: EnvTypes.Production });
